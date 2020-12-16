675995d59f9799ac328e97daea9e43c75b45adc9
Denis Korkh <deniskorh@gmail.com>
2020-12-16 19:10:57 +0200

    Composer autoload refactored

--- config/init.php
+++ config/init.php
@@ -6,9 +6,9 @@ define("ROOT_DIR", dirname(__DIR__));
 define("APP_DIR", ROOT_DIR . "/app");
 define("PUBLIC_DIR", ROOT_DIR . "/public");
 define("CONF_DIR", ROOT_DIR . "/config");
-define("VIEW_DIR", APP_DIR . "/views");
-define("STATICS_DIR", APP_DIR . "/statics");
-define("INSPECTORS_DIR", APP_DIR . "/inspectors");
+define("VIEW_DIR", APP_DIR . "/Views");
+define("STATICS_DIR", APP_DIR . "/Statics");
+define("INSPECTORS_DIR", APP_DIR . "/Inspectors");
 define("CORE_DIR", ROOT_DIR . "/vendor/xuborx/framework/core");
 
 $appPath = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
@@ -20,4 +20,5 @@ define("APP_PATH", $appPath);
 define("APP_ADMIN_PATH", APP_PATH . '/admin');
 
 require_once ROOT_DIR . '/vendor/autoload.php';
+require_once CORE_DIR . '/dev/devFunctions.php';
 require_once CONF_DIR . '/routes.php';
